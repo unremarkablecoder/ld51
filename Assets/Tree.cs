28fccd5dcf2b5bcb895862f14388f750e9854e96
Pontus Eriksson <pontus.eriksson@gmail.com>
2022-10-02 22:54:25 +0200

    smaller tree

--- Assets/Tree.cs
+++ Assets/Tree.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Tree : MonoBehaviour
 {
     public void SetSize(float size) {
-        float scale = 0.5f + 0.1f * size;
+        float scale = 0.5f + 0.05f * size;
         transform.localScale = Vector3.one * scale;
     }
 }
